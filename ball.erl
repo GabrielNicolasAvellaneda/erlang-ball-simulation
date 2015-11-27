cb84b60b81c5da25faf0bfe68568909d6ed1ed51
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-27 11:21:26 -0300

    A game process now control the game state.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- ball.erl
+++ ball.erl
@@ -4,10 +4,51 @@
 -include_lib("wx/include/wx.hrl").
 -include_lib("wx/src/wxe.hrl").
 
+-define(GAME_PROCESS_INSTANCE, game_instance).
+-define(UI_PROCESS_INSTANCE, ui_instance).
+
+%% Use a process to manage game state.
+%% Use a process to manage ui.
+%% The ui process should get updates from the game process with the data to draw.
+%% Use another process to update game object position in auto refresh mode.
+
+start() ->
+	GamePid = spawn(fun init/0),
+	register(?GAME_PROCESS_INSTANCE, GamePid).
+
+init() ->
+	Rectangle = {10, 10, 100, 100},
+	loop(Rectangle).
+
+update_width(Fun, {X, Y, Width, Height}) ->
+	{X, Y, Fun(Width), Height}.	
+
+update_height(Fun, {X, Y, Width, Height}) ->
+	{X, Y, Width, Fun(Height)}.
+
+add1(X) -> X + 1.
+
+update_rectangle(Rectangle) ->
+	update_height(fun add1/1, update_width(fun add1/1, Rectangle)).
+
+loop(State) ->
+	UpdatedState = receive
+		{From, get_state} ->
+			io:format("get_state called from : ~p~n", [From]), 	       
+			From ! {self(), State},
+			State
+		after 30 ->
+			update_rectangle(State)
+		end,
+	loop(UpdatedState).
+
 paint(Wx = #wx{obj=Obj}, WxRef) ->
 	DC = wxPaintDC:new(Obj),
-	wxPaintDC:drawRectangle(DC, {10, 10, 100, 100}),
-	io:format("Received paint event ~p ~p~p~n", [Wx, WxRef, DC]).
+	?GAME_PROCESS_INSTANCE ! {self(), get_state},
+	receive
+		{From, Rectangle} -> io:format("Received ~p~n", [Rectangle]),
+		wxPaintDC:drawRectangle(DC, Rectangle)
+	end.
 
 run() ->
 	Wx = wx:new(),
@@ -19,5 +60,9 @@ run() ->
 
 game_loop(Frame) ->
 	timer:sleep(30),
-	wxFrame:refresh(Frame),
+	repaint(Frame),
 	game_loop(Frame).
+
+repaint(Frame) ->
+	wxFrame:refresh(Frame).
+
