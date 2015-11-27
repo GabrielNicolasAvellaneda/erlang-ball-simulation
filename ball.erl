fd9711f74af24f94d5c4b14156c82641de078f48
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-26 23:55:43 -0300

    Added wx record definitions and paint callback handler.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- ball.erl
+++ ball.erl
@@ -1,7 +1,16 @@
 -module(ball).
 -compile(export_all).
 
+-include_lib("wx/include/wx.hrl").
+-include_lib("wx/src/wxe.hrl").
+
+paint(#wx{}, #wx_ref{}) ->
+	io:format("Received paint event~n").
+
 run() ->
 	Wx = wx:new(),
 	Frame = wxFrame:new(Wx, -1, "Ball simulation"),
-	wxFrame:show(Frame).
+	wxFrame:show(Frame),
+	OnPaint = fun paint/2,
+	wxFrame:connect(Frame, paint, [{callback, OnPaint}]), 
+	Frame.
